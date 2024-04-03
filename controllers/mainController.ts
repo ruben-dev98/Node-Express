fbc541f3932afde4be6191580a099c02629e5a72
ruben.dopico.dev <ruben.dopico.dev@gmail.com>
2024-04-03 20:02:58 +0200

    ADD: spec.json to use rapidoc

--- controllers/mainController.ts
+++ controllers/mainController.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
+import path from 'path';
 
 export const mainRouter = express.Router();
 
 mainRouter.get('/', (_req: Request, res: Response) => {
-    res.sendFile(__dirname + '/public/index.html', (err) => console.error(err));
+    res.sendFile(path.resolve(process.cwd(), '/public/index.html'), (err) => console.error(err));
 });
