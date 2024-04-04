d4c7e61f17648a12ae97318dec9181f6511677f9
ruben.dopico.dev <ruben.dopico.dev@gmail.com>
2024-04-04 09:57:46 +0200

    ADD: igonre .serverless

--- controllers/mainController.ts
+++ controllers/mainController.ts
@@ -4,5 +4,5 @@ import path from 'path';
 export const mainRouter = express.Router();
 
 mainRouter.get('/', (_req: Request, res: Response) => {
-    res.sendFile(path.resolve(process.cwd(), '/public/index.html'), (err) => console.error(err));
+    res.sendFile(path.resolve(process.cwd(), 'index.html'), (err) => console.error(err));
 });
